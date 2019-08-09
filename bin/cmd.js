2160d9dbb76500f7e138d73d1fd6235cb5f9939c
azu <azuciao@gmail.com>
2019-08-09 22:27:49 +0900

    remove autoscale: true

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,45 +1,46 @@
 #!/usr/bin/env node
 
 var cmd = require("../package.json").name;
-var optionator = require('optionator')({
-    prepend: 'Usage:' + cmd + ' [options]',
-    append: 'Version ' + require("../package.json").version,
+var optionator = require("optionator")({
+    prepend: "Usage:" + cmd + " [options]",
+    append: "Version " + require("../package.json").version,
     options: [
         {
-            option: 'help',
-            alias: 'h',
-            type: 'Boolean',
-            description: 'displays help'
+            option: "help",
+            alias: "h",
+            type: "Boolean",
+            description: "displays help"
         },
         {
-            option: 'slide-url',
-            type: 'String',
-            description: 'slide url',
-            example: cmd + ' --slide-url https://azu.github.io/slide/example/slide.html'
+            option: "slide-url",
+            type: "String",
+            description: "slide url",
+            example: cmd + " --slide-url https://azu.github.io/slide/example/slide.html"
         },
         {
-            option: 'base-url',
-            type: 'String',
-            description: 'slide-pdf.js base-url',
-            example: cmd + ' --base-url http://azu.github.io/slide-pdf.js/'
+            option: "base-url",
+            type: "String",
+            description: "slide-pdf.js base-url",
+            example: cmd + " --base-url http://azu.github.io/slide-pdf.js/"
         },
         {
-            option: 'pdf-url',
-            type: 'String',
-            description: 'pdf file path',
-            example: cmd + ' --pdf http://azu.github.io/slide/DOMQuery/sourcemap.pdf'
+            option: "pdf-url",
+            type: "String",
+            description: "pdf file path",
+            example: cmd + " --pdf http://azu.github.io/slide/DOMQuery/sourcemap.pdf"
         },
         {
-            option: 'markdown',
-            type: 'String',
-            description: 'markdown file path',
-            example: cmd + ' --markdown path/to/markdown.md'
-        }, {
-            option: 'output',
-            alias: 'o',
-            type: 'String',
-            description: 'output file path',
-            example: cmd + ' --output index.html'
+            option: "markdown",
+            type: "String",
+            description: "markdown file path",
+            example: cmd + " --markdown path/to/markdown.md"
+        },
+        {
+            option: "output",
+            alias: "o",
+            type: "String",
+            description: "output file path",
+            example: cmd + " --output index.html"
         }
     ]
 });
@@ -47,5 +48,5 @@ var options = optionator.parse(process.argv);
 if (options.help) {
     console.log(optionator.generateHelp());
 } else {
-    require("../lib/writer")(options)
+    require("../lib/writer")(options);
 }
--- public/index.js
+++ public/index.js
@@ -2,7 +2,7 @@
 "use strict";
 document.getElementById("main-slide").focus();
 var fullScreenButton = document.getElementById("js-fullscreen-button");
-fullScreenButton.addEventListener("click", function () {
+fullScreenButton.addEventListener("click", function() {
     var content = document.querySelector(".main-content");
     content.classList.toggle("slide-state-fullscreen");
 });
